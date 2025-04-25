3d71fc1f155594b4021fb293e7d0d632044af903
Bradley Latreille <bradltr95@gmail.com>
2025-04-25 19:43:45 -0400

    task: Set up the project structure to run and display

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# spring-boot
-Spring boot project for writing APIs and connecting to a DB using Docker.
+# Spring-Boot Application for Learning
+Spring boot project for writing APIs, using DBs and Docker.
+
+# Starting the Project
+The spring boot application can be started from
+/src/main/java/com.bradltr95/Application
+
+# Dependencies Managed by Gradle
+Manage dependencies in the /settings.gradle file
+
+@TODO LIST
+- Add Linting.
+- Connect build and tests to Github Actions.
--- src/main/java/com/bradltr95/Application.java
+++ src/main/java/com/bradltr95/Application.java
@@ -2,12 +2,19 @@ package com.bradltr95;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 
+	@GetMapping
+	public String basicRequest() {
+		return "Main application GET request response";
+	}
 }
