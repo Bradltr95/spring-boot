75b5c6b1c7b28bdcec328f7c8637390faa14b17b
Bradley Latreille <bradltr95@gmail.com>
2025-05-03 23:51:56 -0400

    test: include data for our delete test

--- src/test/java/com/bradltr95/SoftwareEngineerDeleteTest.java
+++ src/test/java/com/bradltr95/SoftwareEngineerDeleteTest.java
@@ -1,15 +1,34 @@
 package com.bradltr95;
 
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.when;
 
 public class SoftwareEngineerDeleteTest {
+    @BeforeMethod
+    public void
+    createEngineerRecord() {
+        // This consumes unecessary resources and will get
+        // removed once we package our tests together in a way
+        // that this redudent call can be removed.
+
+        // For now, it assists in creating the data necessary
+        // To get the tests running.
+        Response response = RestAssured.given().
+            body("{\"name\":\"Alice\",\"techStack\":\"Java, Spring\"}").
+            contentType("application/json").
+            when().
+            post("/api/v1/software-engineers");
+    }
+
     @Test public void
     delete_existing_engineer_record() {
         when().
-            delete("/api/v1/software-engineers/1").
+        delete("/api/v1/software-engineers/1").
         then().
-            statusCode(200);
+        statusCode(STATUS_CODE.SUCCESS);
     }
 }
